48a00d04bd7bd65bd3380dadf702cb6771ecf7ae
Prateek-16 <prateekverma54321@gmail.com>
2024-07-22 15:32:38 +0530

    Initial commit

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,10 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+import './Footer.css'
+const Footer = () => {
+  return (
+    <div className='Footer'><p>Copyright @2024, Cryptoplace - All rights reserved.</p></div>
+  )
+}
+
+export default Footer
